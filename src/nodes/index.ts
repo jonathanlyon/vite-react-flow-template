1cea4d71e1d578ee43f0af98d91e6d70c5d86e31
Jonathan Lyon <jonathan.lyon@gmail.com>
2024-08-05 05:40:27 +0000

    deleted node types

--- src/nodes/index.ts
+++ src/nodes/index.ts
@@ -6,13 +6,11 @@ import { AppNode } from "./types";
 export const initialNodes: AppNode[] = [
   {
     id: "a",
-    type: "input",
     position: { x: 0, y: 0 },
     data: { label: "PrettyGood AaaP" },
   },
   {
     id: "b",
-    type: "output",
     position: { x: -100, y: 100 },
     data: { label: "core elements" },
   },
@@ -23,7 +21,6 @@ export const initialNodes: AppNode[] = [
   },
   {
     id: "d",
-    type: "output",
     position: { x: 0, y: 200 },
     data: { label: "Features" },
   },
